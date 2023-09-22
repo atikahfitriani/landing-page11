47c3be6451cd021ddefb35f3432876ddc11e9723
atikahfitriani <145572733+atikahfitriani@users.noreply.github.com>
2023-09-22 23:55:01 +0700

    Add files via upload

---
+++ Landing Page Skilvul/script.js
@@ -0,0 +1,79 @@
+const buttons = document.querySelectorAll(".button");
+const articles = document.querySelectorAll(".card-artikel");
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+   
+    buttons.forEach((btn) => btn.classList.remove("active"));
+
+    button.classList.add("active");
+
+    const category = button.getAttribute("data-category");
+
+    
+    articles.forEach((card) => {
+      if (
+        category === "all" ||
+        category === card.getAttribute("data-category")
+      ) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
+
+function handleGetFormData() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const city = document.getElementById("city").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+
+  return {
+    name,
+    email,
+    city,
+    zipCode,
+    status,
+  };
+}
+
+function isNumber(inputString) {
+  return !isNaN(inputString);
+}
+
+function checkboxIsChecked() {
+  return document.getElementById("status").checked;
+}
+
+function validateFormData(formData) {
+  if (
+    formData &&
+    isNumber(formData.zipCode) &&
+    checkboxIsChecked(formData.status)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function submit() {
+  const formData = handleGetFormData();
+  const isFormValid = validateFormData(formData);
+  const warning = document.getElementById("warning");
+  if (!isFormValid) {
+    warning.style.visibility = "visible";
+    warning.innerHTML = "Periksa form anda sekali lagi";
+  } else {
+    warning.style.visibility = "hidden";
+    warning.innerHTML = "";
+  }
+}
+
+document.getElementById("submit-form").addEventListener("click", function (e) {
+  e.preventDefault();
+  submit();
+});
